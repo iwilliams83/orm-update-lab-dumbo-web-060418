a9884ab6335e10ca8d1cf5ec96b311e81e78b24e
iwilliams83 <indirajamurath@gmail.com>
2018-06-13 16:08:01 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -38,8 +38,7 @@ class Student
   end
 
   def self.new_from_db(row)
-    new_student = self.new(row[1],row[2])
-    new_student.id = row[0]
+    new_student = self.new(row[0],row[1],row[2])
     new_student
   end
 
